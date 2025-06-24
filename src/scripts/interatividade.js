1a42d3c5b64b62d09f7a6f77694d7990ba6e9dc3
Vitoria Rodrigues Hmeliowsky <oliveirawork12@gmail.com>
2025-06-24 13:28:24 -0300

    correção e melhoria da aba inicio e sobre

--- src/scripts/interatividade.js
+++ src/scripts/interatividade.js
@@ -3,34 +3,38 @@ const content = document.getElementById('content');
 
 const menuItens = document.querySelectorAll('#menu li');
 
-//          Objeto para cada página do SPA - dentro de tag main
+//          Objeto para cada pá gina do SPA - dentro de tag main
 const objetoPages = {
   home: `
     <div id="home">
       <h2 class="menus">Seja bem vindo ao meu portfólio</h2>
       <p>Página inicial do projeto - Portfólio pessoal</p>
       <p>SPA estruturado e estilizado com html e css, e interatividade feita com javascript puro.</p>
+
       <h2>Estudante Java</h2>
       <p>Futuro dev backend</p>
-      <h3 id="title-iframe">Trabalhos feitos durante estudos</h3>
-      <div id="iframe">
-      <iframe 
-      title="vimeo-player" 
-      src="https://player.vimeo.com/video/1094858468?h=c07fed689f&autoplay=1&muted=1&loop=1&background=1" 
-      width="640" 
-      height="360" 
-      frameborder="0" 
-      allow="autoplay; fullscreen" 
-      allowfullscreen>
-      </iframe>
-      </div>
-      <a href="https://www.youtube.com/watch?v=9DkyozMdlEc&feature=youtu.be" target="_blank">Acessar o vídeo pelo youtube</a>
+      
+      
+      
       <h2 id="title-img">Ferramentas usadas</h2>
       <div id="langs">
-        <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank"><img src="assets/imgs/html.png" alt="simbolo do html"></a>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank"><img src="assets/imgs/css.png" alt="simbolo do css"></a>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"><img src="assets/imgs/js.png" alt="simbolo do javascript"></a>
+      <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank"><img src="assets/imgs/html.png" alt="simbolo do html"></a>
+      <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank"><img src="assets/imgs/css.png" alt="simbolo do css"></a>
+      <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"><img src="assets/imgs/js.png" alt="simbolo do javascript"></a>
       </div>
+      <h3 id="title-iframe">Exercícios feitos durante estudos</h3>
+      <div id="iframe">
+      <iframe 
+        title="vimeo-player" 
+        src="https://player.vimeo.com/video/1094858468?h=c07fed689f&autoplay=1&muted=1&loop=1&background=1" 
+        width="640" 
+        height="360" 
+        frameborder="0" 
+        allow="autoplay; fullscreen" 
+        allowfullscreen>
+        </iframe>
+        </div>
+        <a id="link" href="https://www.youtube.com/watch?v=9DkyozMdlEc&feature=youtu.be" target="_blank">Acessar o vídeo pelo youtube</a>
     </div>
   `,
   sobre: `
@@ -62,14 +66,60 @@ const objetoPages = {
           <p>Curioso desde 2009 em computação, hoje cursando ADS (Análise e Desenvolvimento de Sistemas) sou entusiasta em tecnologias, buscando sempre por conta própria aprender e/ou me aprimorar.<br>
           recentemente iniciei minha jornada de estudos em Java e POO, após passar pelo frontend, afim de ingressar no mercado como desenvolvedor backend java</p>
 
-          <p>Atualmente estudando JAVA, sou apaixonado por tecnologia, e entusiasta em aprender e solucionar problemas, tenho como objetivo continuar a me aperfeiçoar, e me tornar um desenvolvedor e contribuir com o desenvolvimento e de aplicações, em projetos de sucessos.</p>
+          <p>Atualmente estudando Java, sou apaixonado por tecnologia, e entusiasta em aprender e solucionar problemas, tenho como objetivo continuar a me aperfeiçoar, e me tornar um desenvolvedor e contribuir com o desenvolvimento e de aplicações, em projetos de sucessos.</p>
+          
+          <h2>Leque de ferramentas atuais</h2>
+            <div id="leque-tools">
+              <img src="assets/imgs/htmlico.png" alt="icone do html">
+              <img src="assets/imgs/cssico.png" alt="icone do css">
+              <img src="assets/imgs/jsico.png" alt="icone do javascript">
+              <img src="assets/imgs/nodeico.png" alt="icone do node">
+              <img src="assets/imgs/pyico.png" alt="icone do python">
+              <img src="assets/imgs/gitico.png" alt="icone do git">
+              <img src="assets/imgs/mysqlico.png" alt="icone do mysql">
+              <img src="assets/imgs/linuxico.png" alt="icone do linux">
+            </div>
         </div>
       </div>
     </div>
   `,
   projetos: `
-    <h2 class="menus">Último projeto</h2>
-    <p>último projeto aqui o link</p>
+    <div id="projetos">
+      <div>
+        <h2>Projetos desenvolvidos com html, css e javascript</h2>
+        <p>Projetos apenas com frontend básico</p>
+      </div>
+
+      <div id="projetos-wrap">
+        <div class="content projeto-1">
+          <div>
+            <img src="assets/imgs/projeto_1.png" alt="print do projeto portal de notícias">
+            <p>Portal de notícias</p>
+            <a href="">Ver página no github pages</a>
+            <a href="">Ver código no repositório</a>
+          </div>
+        </div>
+
+        <div class="content projeto-2">
+          <div>
+            <img src="assets/imgs/projeto_2.png" alt="print do projeto login vintage">
+            <p>Projeto login vintage</p>
+            <a href="">Ver página no github pages</a>
+            <a href="">Ver código no repositório</a>
+          </div>
+        </div>
+
+        <div class="content projeto-3">
+          <div>
+            <img src="assets/imgs/projeto_3.png" alt="print do projeto local turístico">
+            <p>Projeto local turístico</p>
+            <a href="">Ver página no github pages</a>
+            <a href="">Ver código no repositório</a>
+          </div>
+        </div>
+
+      </div>
+    </div>
 
   `,
   contato: `
@@ -83,13 +133,13 @@ const objetoPages = {
 
 //          Captura o clique nos itens do menu
 function reloadPage(page) {
-  content.innerHTML = objetoPages[page] + objetoPages.footer;
+  content.innerHTML = objetoPages[page] +  objetoPages.footer;
 }
 
 //          Captura o clique nos itens do menu
 menuItens.forEach(item => {
   item.addEventListener('click', () => {
-    const page = item.getAttribute('data-page');
+    const page = item.getAttribute ('data-page');
     reloadPage(page);
   });
 });
@@ -97,4 +147,4 @@ menuItens.forEach(item => {
 
 
 //          Carregar a página inicial
-reloadPage('home');
+reloadPage ('projetos');
